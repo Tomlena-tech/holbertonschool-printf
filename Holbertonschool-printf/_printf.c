5b3b6225d40fb7e4a96417a7905d41e488da3893
Thomas <decourttthomas@gmail.com>
2025-07-21 11:56:40 +0200

    Création du Repositary Holbertonschool-printf

---
+++ Holbertonschool-printf/_printf.c
@@ -0,0 +1,12 @@
+#include <stdarg.h>
+#include "holberton.h"
+#include "main.h"
+
+/**
+ * _printf - Custom printf function
+ * @format: The format string
+ * @...: The values to format and print
+ *
+ * Return: The number of characters printed
+ */
+ 
