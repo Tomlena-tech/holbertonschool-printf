b3e6299c30dbbd03432ea3b4e7e4521a186763d9
root <dorian.oufer@gmail.com>
2025-07-21 11:57:14 +0000

    Ajout biblioteques et creation premier proto

--- _printf.h
+++ _printf.h
@@ -1,1 +1,11 @@
-#include
+#ifndef _PRINTF.H
+#define _PRINTF.H
+
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+int _printf_char(char c);
+int _printf_char(char *s);
+
+#endif
