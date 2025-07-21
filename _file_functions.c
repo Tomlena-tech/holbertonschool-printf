2247487f25026f479990a5562e11abc93f4e7147
Thomas <decourttthomas@gmail.com>
2025-07-21 16:16:23 +0200

    Création des fonctions print_char & print_str

---
+++ _file_functions.c
@@ -0,0 +1,37 @@
+#include "_printf.h"
+#include <stdarg.h>
+/**
+ * print_char - Imprime un caractere
+ * @args: les arguments.
+
+ * Return: Le caractère imprimé
+ */
+
+
+ int print_char(va_list args)
+{
+    _putchar(va_arg(args, int));
+    return (1);
+}
+
+/**
+ * print_str - Imprime une chaîne de caractères
+ * @args: les arguments.
+ *
+ * Return: le nombre de caractères imprimés
+ */
+
+int print_str(va_list args)
+{
+
+	int j;
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+		str = "(null)";
+	for (j = 0; str[j]; j++)
+	{
+		_putchar(str[j]);
+	}
+	return (j);
+}
